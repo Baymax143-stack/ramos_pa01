a081f80dcabb2f6700b7453f01873b087490a8e0
Baymax143-stack <59593004+Baymax143-stack@users.noreply.github.com>
2020-01-18 19:37:35 -0500

    first commit

---
+++ src/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package ramos_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%s", "Goodbye, World!");
+	}
+
+}
